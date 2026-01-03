435a735b45c29e977177b2d6360603a61a516ca1
Gitea <gitea@fake.local>
2026-01-03 17:24:36 +0800

    temp 20260103

--- quick_infer.sh
+++ quick_infer.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 MODEL_PATH="/storage/v-jinpewang/az_workspace/wenjun/Qwen-Image2/my_hf_cache/Qwen-Image-Edit"
-LORA_PATH="./result/checkpoint-500"
-INPUT_IMG="./control_images2/001.jpg"
+LORA_PATH="./result4/checkpoint-3000"
+INPUT_IMG="./test2.jpg"
 OUTPUT_DIR="./outputs"
 
 RESOLUTION=$((512*512))
@@ -12,7 +12,7 @@ python scripts/quick_infer.py \
     --lora_weight "$LORA_PATH" \
     --ctrl_img "$INPUT_IMG" \
     --output_img "$OUTPUT_DIR/result_$(date +%Y%m%d_%H%M%S).png" \
-    --prompt "Put a necklace on him" \
+    --prompt "Edit the image according to the text instruction in the image" \
     --cfg_scale 6.0 \
     --infer_steps 50 \
     --target_area $RESOLUTION \
